bab89ccce6a7c44726eeeb4b4ab630fe969daf81
Alisson <alissondbass@gmail.com>
2022-08-26 20:53:15 -0300

    agencia de turismo

--- tarefa-dia-2/pratica-1.js
+++ tarefa-dia-2/pratica-1.js
@@ -2,10 +2,13 @@ var nome = 'Alisson';
 var email = 'alissondbass@gmail.com';
 var profissao = 'desenvolvedor';
 var idade = 27;
-var destino = 'argentina';
+var destino = 'Argentina';
 
-console.log("nome: " +  nome);
-console.log("email: " +  email);
-console.log("profissao: " + profissao);
-console.log("idade: " + idade);
-console.log("destino: " + destino);
+// console.log("nome: " +  nome);
+// console.log("email: " +  email);
+// console.log("profissao: " + profissao);
+// console.log("idade: " + idade);
+// console.log("destino: " + destino);
+
+
+console.log(`Olá, ${nome}! Somos aqui da agência de turismo Viagem dos Sonhos. Estamos te escrevendo este email, pois acabamos de confirmar as compras das passagens aéreas para ${destino}. Confirma pra gente alguns dados? A sua idade é ${idade}, você é ${profissao} e o seu email é ${email}? Ficamos no aguardo. Muito obrigado e boa viagem! :D`);
