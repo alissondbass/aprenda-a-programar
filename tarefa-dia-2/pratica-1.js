4153418cf54dc77d03d247fdd3ea7c9220db838b
Alisson <alissondbass@gmail.com>
2022-08-25 21:16:17 -0300

    teste console.log()

--- tarefa-dia-2/pratica-1.js
+++ tarefa-dia-2/pratica-1.js
@@ -3,3 +3,9 @@ var email = 'alissondbass@gmail.com';
 var profissao = 'desenvolvedor';
 var idade = 27;
 var destino = 'argentina';
+
+console.log("nome: " +  nome);
+console.log("email: " +  email);
+console.log("profissao: " + profissao);
+console.log("idade: " + idade);
+console.log("destino: " + destino);
