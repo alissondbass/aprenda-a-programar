a72501d35fc20fc4cf7c30c9f84083b6e795d99e
Alisson <alissondbass@gmail.com>
2022-08-27 00:28:42 -0300

    tarefas finalizadas

--- tarefa-dia-2/pratica-4.js
+++ tarefa-dia-2/pratica-4.js
@@ -1,4 +1,4 @@
-// Se o produto for internacional aplicar 20% de impostos. E, se for nacional aplicar 12% de desconto.
+// Se o produto for internacional aplicar 20% de impostos. E, se for nacional aplicar 12% de imposto.
 
 let produtoA = {"nome": "camiseta", "valor": 100.00, "internacional": true};
 let produtoB = {"nome": "perfume", "valor": 200.00, "internacional": true};
@@ -15,7 +15,7 @@ console.log(`Lista de produtos \n\n${produtoA["nome"]}; \n${produtoB["nome"]}; \
 if(produtoA.internacional == true){
 
     console.log(`O produto ${produtoA.nome} é um item de origem internacional, por isso o valor dele R$${produtoA.valor} acrescido ao imposto do Brasil é de R$${produtoA.valor*impostoInternacional}.`);
-   
+
     } else {
 
         console.log(`O produto ${produtoA.nome} é um item de origem nacional, por isso o valor dele R$${produtoA.valor} acrescido ao imposto do Brasil é de R$${produtoA.valor*impostoNacional}.`);
