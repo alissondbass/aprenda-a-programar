fe8120d8dbaa7e8c02ee9e2f54aa06d137c243e4
Alisson <alissondbass@gmail.com>
2022-08-28 17:32:02 -0300

    tabuada

---
+++ tarefa-dia-3/pratica-2.js
@@ -0,0 +1,15 @@
+// Crie uma tabuada.    
+
+let n = 2;
+let multiplicador = 1;
+
+while(multiplicador <= 9){
+
+    multiplicador*n;
+
+    console.log(`\n${n} X ${multiplicador} = ${multiplicador*n}`);
+
+    multiplicador++;
+
+
+}
