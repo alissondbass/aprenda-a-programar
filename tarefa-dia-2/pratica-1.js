b16818f7ec63beffebf18992d584fd39a6202e80
Alisson <alissondbass@gmail.com>
2022-08-28 22:03:12 -0300

    correção pratica-1

--- tarefa-dia-2/pratica-1.js
+++ tarefa-dia-2/pratica-1.js
@@ -1,23 +1,18 @@
-//var nome = 'Alisson';
+var nome = 'Alisson';
 var email = 'alissondbass@gmail.com';
 var profissao = 'desenvolvedor';
 var idade = 27;
 var destino = 'Argentina';
 
-var nome = prompt("Nome");
+
+console.log("\nnome: " +  nome);
+console.log("email: " +  email);
+console.log("profissao: " + profissao);
+console.log("idade: " + idade);
+console.log("destino: " + destino);
 
 
-
-
-
-// console.log("nome: " +  nome);
-// console.log("email: " +  email);
-// console.log("profissao: " + profissao);
-// console.log("idade: " + idade);
-// console.log("destino: " + destino);
-
-
-//console.log(`\nOlá, ${nome}! Somos aqui da agência de turismo Viagem dos Sonhos. 
-//\nEstamos te escrevendo este email, pois acabamos de confirmar as compras das passagens aéreas para ${destino}. 
-//Confirma pra gente alguns dados? A sua idade é ${idade}, você é ${profissao} e o seu email é ${email}? 
-//\nFicamos no aguardo. Muito obrigado e boa viagem! :D`);
+console.log(`\nOlá, ${nome}! Somos aqui da agência de turismo Viagem dos Sonhos. 
+\nEstamos te escrevendo este email, pois acabamos de confirmar as compras das passagens aéreas para ${destino}. 
+Confirma pra gente alguns dados? A sua idade é ${idade}, você é ${profissao} e o seu email é ${email}? 
+\nFicamos no aguardo. Muito obrigado e boa viagem! :D`);
